19e4c74724e854230ccf364cadf20d8b6a0c86e3
Brandon Ladson <brandonladson1999@gmail.com>
2020-11-03 11:07:41 +0100

    revisione codice js post live coding

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-var menuAperto = false;
+// var menuAperto = false;
+//
+// $('a > i').click(function () {
+//
+//   if (menuAperto === false) {
+//     $('.hamburger-menu').show(100);
+//     menuAperto = true;
+//
+//   } else {
+//     $('.hamburger-menu').hide(100);
+//     menuAperto = false;
+//   }
+//
+// });
 
 $('a > i').click(function () {
+  $('.hamburger-menu').addClass("active");
+});
 
-  if (menuAperto === false) {
-    $('.hamburger-menu').show(100);
-    menuAperto = true;
-
-  } else {
-    $('.hamburger-menu').hide(100);
-    menuAperto = false;
-  }
-
+$('a.close > i').click(function () {
+  $('.hamburger-menu').removeClass("active");
 });
